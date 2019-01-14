5d6eaf7a9a9a7aadab287a577f6a8929b9d6f135
Ken-S-0114 <g031n071@s.iwate-pu.ac.jp>
2019-01-14 21:21:22 +0900

    first commit

---
+++ MVP/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  MVP
+//
+//  Created by 佐藤賢 on 2019/01/14.
+//  Copyright © 2019 佐藤賢. All rights reserved.
+//
+
+import UIKit
+import GitHub
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+}
+
